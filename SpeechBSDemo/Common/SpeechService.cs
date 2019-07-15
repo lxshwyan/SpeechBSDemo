a1af7151bb9d778ec153f8a7a3decce2578e9923
lxshwyan <327251174@qq.com>
2019-07-15 12:13:57 +0800

    添加项目文件。

---
+++ SpeechBSDemo/Common/SpeechService.cs
@@ -0,0 +1,53 @@
+﻿/************************************************************************
+* Copyright (c) 2019 All Rights Reserved.
+*命名空间：SpeechBSDemo.Common
+*文件名： SpeechService
+*创建人： Lxsh
+*创建时间：2019/7/15 11:16:04
+*描述
+*=======================================================================
+*修改标记
+*修改时间：2019/7/15 11:16:04
+*修改人：Lxsh
+*描述：
+************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Web;
+
+namespace SpeechBSDemo
+{
+    public class SpeechService
+    {
+        private static SpeechSynthesizer synth = null;
+        /// <summary>
+        /// 返回一个SpeechSynthesizer对象
+        /// </summary>
+        /// <returns></returns>
+        private static SpeechSynthesizer GetSpeechSynthesizerInstance()
+        {
+            if (synth == null)
+            {
+                synth = new SpeechSynthesizer();
+            }
+            return synth;
+        }
+        /// <summary>
+        /// 保存语音文件
+        /// </summary>
+        /// <param name="text"></param>
+        public static void SaveMp3(string strFileName,string spText)
+        {    
+            synth = GetSpeechSynthesizerInstance();
+            synth.Rate = 1;
+            synth.Volume = 100;      
+            synth.SetOutputToWaveFile(strFileName);
+            synth.Speak(spText);
+            synth.SetOutputToNull();
+        }
+
+    }
+}
---
+++ SpeechBSDemo/Controllers/TestSpeechController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpeechBSDemo.Controllers
+{
+    public class TestSpeechController : Controller
+    {
+        // GET: TestSpeech
+        public ActionResult Index()
+        {   
+            return View();
+        }
+        public async Task<ActionResult> PlayWav(string id,string spText)
+        {    
+            string strPath = Server.MapPath("~\\MP4\\" + id + ".wav");
+            SpeechService.SaveMp3(strPath, spText);
+            try
+            {
+                //FTP 直接返回Stream，需要自己实现
+                using (FileStream fileStream = new FileStream(strPath, FileMode.Open))
+                {
+                    byte[] fileByte = new byte[fileStream.Length];
+                    fileStream.Seek(0, SeekOrigin.Begin);
+                    fileStream.Read(fileByte, 0, (int)fileStream.Length);
+                    long fSize = fileStream.Length;
+                    long startbyte = 0;
+                    long endbyte = fSize - 1;
+                    int statusCode = 200;
+                    if ((Request.Headers["Range"] != null))
+                    {
+                        //Get the actual byte range from the range header string, and set the starting byte.
+                        string[] range = Request.Headers["Range"].Split(new char[] { '=', '-' });
+                        startbyte = Convert.ToInt64(range[1]);
+                        if (range.Length > 2 && range[2] != "") endbyte = Convert.ToInt64(range[2]);
+                        //If the start byte is not equal to zero, that means the user is requesting partial content.
+                        if (startbyte != 0 || endbyte != fSize - 1 || range.Length > 2 && range[2] == "")
+                        { statusCode = 206; }//Set the status code of the response to 206 (Partial Content) and add a content range header.                                    
+                    }
+                    long desSize = endbyte - startbyte + 1;
+                    //Headers
+                    Response.StatusCode = statusCode;
+                    Response.ContentType = "audio/mpeg";
+                    Response.AddHeader("Content-Accept", Response.ContentType);
+                    Response.AddHeader("Content-Length", desSize.ToString());
+                    Response.AddHeader("Content-Range", string.Format("bytes {0}-{1}/{2}", startbyte, endbyte, fSize));
+                    return File(fileByte, Response.ContentType);
+                    //return new FileStreamResult(fileByte, Response.ContentType);  这个方法不行
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+       
+    }
+}
